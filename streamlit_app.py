ae4f44b300b0e2039a1034dd9c7e77d30067174a
CR-DataTeam <110788223+CR-DataTeam@users.noreply.github.com>
2024-08-29 02:09:38 +0000

    clean up

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,13 +6,13 @@ import hmac
 import streamlit as st
 st. set_page_config(layout="wide")
 st.logo('usrc_inverted3.png')
-st.title("Resource Allocation 🩺")
-st.write(
-    "Click button below to query current worklist status:"
-)
+#st.title("Resource Allocation 🩺")
+#st.write(
+#    "Click button below to query current worklist status:"
+#)
 
 
-prototype_run = st.button("Get Data")
+prototype_run = st.button("Run Worklist Resourcing")
 
 if prototype_run:
     sf.run_test()
